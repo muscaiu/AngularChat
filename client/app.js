adef97b413022282f83e852a356968d9e5d7c0d7
muscaiu <muscaiu@yahoo.com>
2016-12-12 17:43:28 +0200

    sending username and password from client to server with io

--- client/app.js
+++ client/app.js
@@ -1,17 +1,28 @@
 $(function(){
     //sidenav collapse
     $(".button-collapse").sideNav();
-
+    
+    //declare io() for the client
+    var socket = io();
+    
     //click submit
     $('#submitInput').on('click', function(){
-        console.log($('#usernameInput').val(),$('#passwordInput').val() )
+        username = $('#usernameInput').val().trim()
+        password = $('#passwordInput').val().trim()
+
+        socket.emit('loginData', username, password)
         //send it to the server
     })
+
+
+
+
+
+
+    
 
     //form prevent default
     $('#inputForm').submit(function(event){
         event.preventDefault();
     })
-
-
 })
--- server/server.js
+++ server/server.js
@@ -3,8 +3,27 @@ var app = express();
 var port = process.env.PORT || 3000;
 var server = require('http').createServer(app);
 app.use(express.static(__dirname + '/../client'));
+//add io
+var io = require('socket.io')(server)
+
+//Logged in users
+var varnumUsers = 0;
+
+io.on('connection', function(socket){
+    console.log('user connected')
+
+    socket.on('loginData', function (username, password) {
+        console.log(username, password)
+    });
+
+    // socket.on('userLogin', function(){
+    //     //notify that user is logged in
+
+    //     //change to chatroom
+
+    // })
+})
 
 server.listen(port, function(){
     console.log('Server listening on port %d', port)
-    console.log(__dirname + '/../client')
 })
