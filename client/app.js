b69fea58443f6317100b790167cf5262c5116fd7
muscaiu <muscaiu@yahoo.com>
2016-12-15 17:48:36 +0200

    remade most of the app cooler

--- client/app.js
+++ client/app.js
@@ -1,28 +1,109 @@
-$(function(){
-    //sidenav collapse
-    $(".button-collapse").sideNav();
-    
-    //declare io() for the client
-    var socket = io();
-    
-    //click submit
-    $('#submitInput').on('click', function(){
-        username = $('#usernameInput').val().trim()
-        password = $('#passwordInput').val().trim()
 
-        socket.emit('loginData', username, password)
-        //send it to the server
+// var app = angular.module('ChatApp', ['ngRoute'])
+var app = angular.module('ChatApp', ['ui.router'])
+
+    //UI.ROUTER
+    .config(function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+            .state("chat", {
+                url: "/chat",
+                templateUrl: "chat.html",
+                controller: "ChatController as chat",
+            })
+            .state("chat.login", {
+                url: "/login",
+                templateUrl: "login.html",
+            })
+            .state("chat.register", {
+                url: "/register",
+                templateUrl: "register.html",
+            })
+            .state("chat.chatroom", {
+                url: "/chatroom",
+                templateUrl: "chatroom.html",
+            })
+
+        $urlRouterProvider.otherwise('/chat/login');
     })
 
+    //Services
+    // .service('loggedUsers', function LoggedUsers() {
+    //     var loggedUsers = this;
+    //     loggedUsers.users = []
+    // })
 
+    //LoginController
+    .controller('ChatController', function ChatController($scope) {
+        var socket = io();
 
+        var chat = this;
+        chat.users = [];
 
+        chat.LoginForm = function () {
+            console.log(chat.usernameLogin, chat.passwordLogin)
+            console.log('users: ' + chat.users)
+            socket.emit('login attempt', chat.usernameLogin, chat.passwordLogin)
+        }
+        chat.RegisterForm = function(){
+            socket.emit('register user', chat.usernameRegister, chat.passwordRegister)
+            console.log('register')
+        }
 
-
-    
-
-    //form prevent default
-    $('#inputForm').submit(function(event){
-        event.preventDefault();
+        socket.on('user logged in', function (data) {
+            $scope.$apply(function () {
+                chat.users.push(data);
+                console.log('logged in users: ' + chat.users)
+            })
+        })
     })
-})
+
+//sidenav collapse
+$(".button-collapse").sideNav();
+
+
+
+// var app = angular.module('ChatApp', ['ui.router'])
+
+//     //UI.ROUTER
+//     .config(function config($stateProvider, $urlRouterProvider) {
+//         // $stateProvider
+//         //     .state('home', {
+//         //         controller: "HomeController as home",
+//         //         url: '',
+//         //         templateUrl: 'navbar.html'
+//         //         // views: {
+//         //         //     nav: {
+//         //         //         templateUrl: 'navbar.html'
+//         //         //     }
+//         //         // }
+//         //     })
+
+    // //Routing ngRoute
+    // .config(function ($routeProvider) {
+    //     $routeProvider
+    //         .when('/login', {
+    //             controller: 'LoginController',
+    //             templateUrl: 'login.html'
+    //         })
+    //         .when('/register', {
+    //             controller: 'RegisterController',
+    //             templateUrl: 'register.html'
+    //         })
+    //         .otherwise({
+    //             redirectTo: '/login'
+    //         })
+    // })
+
+    // .service('sharedProperties', function () {
+    //     var property = '1'
+    //     return {
+    //         getProperty: function () {
+    //             return property;
+    //         },
+    //         setProperty: function (value) {
+    //             property = value;
+    //         }
+    //     };
+    // })
--- server/server.js
+++ server/server.js
@@ -2,28 +2,64 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var server = require('http').createServer(app);
-app.use(express.static(__dirname + '/../client'));
-//add io
+var mongoose = require('mongoose')
 var io = require('socket.io')(server)
 
-//Logged in users
-var varnumUsers = 0;
-
-io.on('connection', function(socket){
-    console.log('user connected')
-
-    socket.on('loginData', function (username, password) {
-        console.log(username, password)
-    });
-
-    // socket.on('userLogin', function(){
-    //     //notify that user is logged in
-
-    //     //change to chatroom
-
-    // })
+//User model for mongoose
+var User = mongoose.model('user', {
+    username: String,
+    password: String
 })
 
-server.listen(port, function(){
+//logged in users
+var varnumUsers = 0;
+
+//io
+io.on('connection', function (socket) {
+    //When a request to the api is being done, console.log GetMessages
+    console.log('user connected')
+
+    //start API
+    app.get('/api/user', GetUsers)
+    function GetUsers(req, res) {
+        User.find({}).exec(function (err, result) {
+            console.log(result)
+            res.send(result)
+        })
+    }
+
+    socket.on('register user', function (username, password) {
+        console.log('regUser: '+ username, 'regPassword: ' + password)
+        //Save user in MongoDB
+        var user = new User({
+            username: username,
+            password: password
+        })
+        user.save()
+
+        //io.sockets.emit('new user', socket.username)
+    });
+
+    socket.on('login attempt', function(username, password){
+        console.log('loginUser: '+ username, 'loginPassword: ' + password)
+        socket.username = username;
+        socket.broadcast.emit('user logged in', socket.username)
+    })
+
+})
+
+//serve files
+app.use(express.static(__dirname + '/../client'));
+
+//start server
+server.listen(port, function () {
     console.log('Server listening on port %d', port)
+})
+
+//conenct to Mongoose
+mongoose.connect('mongodb://localhost:27017/chat', function (err, db) {
+    if (!err) {
+        console.log('connected to mongo')
+    }
+    else (console.log(err))
 })
