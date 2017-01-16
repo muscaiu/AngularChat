c38648799bf6c5e5ac53db9f39a064c1b191643d
Cristian Muscalu <muscaiu@yahoo.com>
2017-01-16 17:21:16 +0200

    got some Log Off functionality, but not 100%

--- client/app.js
+++ client/app.js
@@ -1,130 +1,125 @@
-
 // var app = angular.module('ChatApp', ['ngRoute'])
 var app = angular.module('ChatApp', ['ui.router', 'ngMaterial', 'ngStorage'])
 
-    //UI.ROUTER
-    .config(function config($stateProvider, $urlRouterProvider) {
+//UI.ROUTER
+.config(function config($stateProvider, $urlRouterProvider) {
 
-        $stateProvider
+    $stateProvider
 
-            .state("chat", {
-                url: "/chat",
-                templateUrl: "chat.html",
-                controller: "ChatController as chat",
-            })
-            .state("chat.login", {
-                url: "/login",
-                templateUrl: "login.html",
-            })
-            .state("chat.register", {
-                url: "/register",
-                templateUrl: "register.html",
-            })
-            .state("chat.chatroom", {
-                url: "/chatroom",
-                templateUrl: "chatroom.html",
-            })
+        .state("chat", {
+            url: "/chat",
+            templateUrl: "chat.html",
+            controller: "ChatController as chat",
+        })
+        .state("chat.login", {
+            url: "/login",
+            templateUrl: "login.html",
+        })
+        .state("chat.register", {
+            url: "/register",
+            templateUrl: "register.html",
+        })
+        .state("chat.chatroom", {
+            url: "/chatroom",
+            templateUrl: "chatroom.html",
+        })
 
-        $urlRouterProvider.otherwise('/chat/login');
+    $urlRouterProvider.otherwise('/chat/login');
+})
+
+//Services
+// .service('loggedUsers', function LoggedUsers() {
+//     var loggedUsers = this;
+//     loggedUsers.users = []
+// })
+
+//LoginController
+.controller('ChatController', function ChatController($scope, $state, $localStorage) {
+    var socket = io()
+
+    var chat = this
+    chat.users = []
+    chat.messages = []
+    var connected = false
+    $scope.$participants = 0
+    var loggedUser = "Guest"
+
+    $scope.$currentUser = $localStorage.$default({
+        username: 'Guest',
+        token: null
     })
 
-    //Services
-    // .service('loggedUsers', function LoggedUsers() {
-    //     var loggedUsers = this;
-    //     loggedUsers.users = []
-    // })
+    LoadMessages()
 
-    //LoginController
-    .controller('ChatController', function ChatController($scope, $state, $localStorage) {
-        var socket = io()
-
-        var chat = this
-        chat.users = []
-        chat.messages = []
-        var connected = false
-        $scope.$participants = 0
-
-        $scope.$currentUser = $localStorage.$default({
-            username: 'Guest',
-            token: null
-        })
-
-        LoadMessages()
-        function LoadMessages() {
-            $.getJSON('http://localhost:3000/api/message', function (data) {
-                $.each(data, function (key, val) {
-                    addChatMessage(val)
-                })
-            })
-        }
-
-        $scope.clickDisconnect = function () {
-            $scope.$currentUser.$reset()
-            $scope.$currentUser.username = 'Guest'
-        }
-
-        chat.LoginForm = function () {
-            if (chat.usernameLogin == "") {
-                Materialize.toast('Empty fields!', 4000)
-            } else {
-                console.log(chat.usernameLogin, chat.passwordLogin)
-                console.log('users: ' + chat.users)
-                socket.emit('login attempt', chat.usernameLogin, chat.passwordLogin)
-            }
-        }
-
-        chat.RegisterForm = function () {
-            if (chat.passwordRegister === chat.repeatPasswordRegister) {
-                socket.emit('register user', chat.usernameRegister, chat.passwordRegister)
-            }
-            else {
-                Materialize.toast('Passwords don`t match !', 4000)
-                console.log('wrong credentials')
-            }
-        }
-
-        chat.sendMessage = function () {
-            socket.emit('send message', chat.message)
-            chat.message = ''
-        }
-
-        //when the user is succesfully connected by the server
-        socket.on('login', function (data) {
-            connected = true
-            $state.go('chat.chatroom')
-            chat.usernameLogin = ''
-            chat.passwordLogin = ''
-
-            //connect user to token
-            var userToken = Math.random().toString(36).substring(3, 16)// + new Date
-            localStorage.setItem('userToken', userToken)
-            //socket.emit('user token', localStorage.getItem('userToken'))
-            console.log(userToken)
-
-            $scope.$currentUser = {
-                username: data.username,
-                token: userToken
-            }
-
-        })
-
-        //when a user has logged in alert all sockets
-        socket.on('user logged in', function (data) {
-            $scope.$apply(function () {
-                chat.users.push(data.username);
-                $scope.$participants = data.numUsers
-                console.log('logged in users: ' + chat.users)
+    function LoadMessages() {
+        $.getJSON('http://localhost:3000/api/message', function(data) {
+            $.each(data, function(key, val) {
+                addChatMessage(val)
             })
         })
+    }
 
-        socket.on('wrong credentials', function () {
-            Materialize.toast('Wrong credentials !', 4000)
+    chat.LoginForm = function() {
+        if (chat.usernameLogin == "") {
+            Materialize.toast('Empty fields!', 4000)
+        } else {
+            console.log(chat.usernameLogin, chat.passwordLogin)
+            console.log('users: ' + chat.users)
+            socket.emit('login attempt', chat.usernameLogin, chat.passwordLogin)
+        }
+    }
+
+    chat.RegisterForm = function() {
+        if (chat.passwordRegister === chat.repeatPasswordRegister) {
+            socket.emit('register user', chat.usernameRegister, chat.passwordRegister)
+        } else {
+            Materialize.toast('Passwords don`t match !', 4000)
             console.log('wrong credentials')
-        })
+        }
+    }
 
-        //add message the screen
-        var addChatMessage = function (data) {
-            $scope.$apply(function () {
+    chat.sendMessage = function() {
+        socket.emit('send message', chat.message)
+        chat.message = ''
+    }
+
+    //when the user is succesfully connected by the server
+    socket.on('login', function(data) {
+        connected = true
+        $state.go('chat.chatroom')
+        chat.usernameLogin = ''
+        chat.passwordLogin = ''
+
+        //connect user to token
+        var userToken = Math.random().toString(36).substring(3, 16) // + new Date
+        localStorage.setItem('userToken', userToken)
+            //socket.emit('user token', localStorage.getItem('userToken'))
+        console.log(userToken)
+
+        $scope.$currentUser = {
+            username: data.username,
+            token: userToken
+        }
+
+    })
+
+    //when a user has logged in alert all sockets
+    socket.on('user logged in', function(data) {
+        $scope.$apply(function() {
+            chat.users.push(data.username);
+            $scope.$participants = data.numUsers
+            console.log('logged in users: ' + chat.users)
+        })
+    })
+
+    socket.on('wrong credentials', function() {
+        Materialize.toast('Wrong credentials !', 4000)
+        console.log('wrong credentials')
+    })
+
+    //add message the screen
+    var addChatMessage = function(data) {
+            $scope.$apply(function() {
                 chat.messages.push({
                     username: data.username,
                     message: data.message
@@ -132,37 +127,56 @@ var app = angular.module('ChatApp', ['ui.router', 'ngMaterial', 'ngStorage'])
             })
         }
         //send the message to the server
-        socket.on('emit message', function (data) {
-            $scope.$apply(function () {
-                chat.messages.push({
-                    username: data.username,
-                    message: data.message
-                });
-            })
-        })
-
-        socket.on('user left', function (data) {
-            $scope.$apply(function () {
-                chat.users.splice(-1, 1)
-                $scope.$participants = data.numUsers
-                //chat.users.push(data.username);
-            })
-            console.log(data.username + ' left')
-        })
-
-        socket.on('disconnect', function () {
-            Materialize.toast('Disconnected', 4000)
-        })
-        socket.on('reconnect', function () {
-            Materialize.toast('Connected', 4000)
-        })
-        socket.on('reconnect_error', function () {
-            Materialize.toast('Attempt to reconnect failed', 4000)
+    socket.on('emit message', function(data) {
+        $scope.$apply(function() {
+            chat.messages.push({
+                username: data.username,
+                message: data.message
+            });
         })
     })
 
-$(document).on('click', '#toast-container .toast', function () {
-    $(this).fadeOut(function () {
+    socket.on('user left', function(data) {
+        $scope.$apply(function() {
+            _.pull(chat.users, data.username)
+            $scope.$participants = data.numUsers
+                //chat.users.push(data.username);
+        })
+        console.log(data.username + ' left')
+    })
+
+    //Call Logg off
+    $scope.LogOff = function() {
+        socket.emit('log off')
+    }
+
+    //Execute Log off from server
+    socket.on('log off event', function(numSockets, numUsers) {
+        //Removing current user from array with Loadash
+        _.pull(chat.users, $scope.$currentUser.username)
+            // $scope.$apply(function() {
+            //         $scope.$currentUser = {
+            //             username: "Guest",
+            //             token: null
+            //         }
+            //     })
+            //console.log($currentUser.username)
+        console.log('logged in users: ' + chat.users)
+    })
+
+    socket.on('disconnect', function() {
+        Materialize.toast('Disconnected', 4000)
+    })
+    socket.on('reconnect', function() {
+        Materialize.toast('Connected', 4000)
+    })
+    socket.on('reconnect_error', function() {
+        Materialize.toast('Attempt to reconnect failed', 4000)
+    })
+})
+
+$(document).on('click', '#toast-container .toast', function() {
+    $(this).fadeOut(function() {
         $(this).remove();
     });
 
@@ -188,30 +202,30 @@ $(document).on('click', '#toast-container .toast', function () {
 //         //         // }
 //         //     })
 
-    // //Routing ngRoute
-    // .config(function ($routeProvider) {
-    //     $routeProvider
-    //         .when('/login', {
-    //             controller: 'LoginController',
-    //             templateUrl: 'login.html'
-    //         })
-    //         .when('/register', {
-    //             controller: 'RegisterController',
-    //             templateUrl: 'register.html'
-    //         })
-    //         .otherwise({
-    //             redirectTo: '/login'
-    //         })
-    // })
+// //Routing ngRoute
+// .config(function ($routeProvider) {
+//     $routeProvider
+//         .when('/login', {
+//             controller: 'LoginController',
+//             templateUrl: 'login.html'
+//         })
+//         .when('/register', {
+//             controller: 'RegisterController',
+//             templateUrl: 'register.html'
+//         })
+//         .otherwise({
+//             redirectTo: '/login'
+//         })
+// })
 
-    // .service('sharedProperties', function () {
-    //     var property = '1'
-    //     return {
-    //         getProperty: function () {
-    //             return property;
-    //         },
-    //         setProperty: function (value) {
-    //             property = value;
-    //         }
-    //     };
-    // })
+// .service('sharedProperties', function () {
+//     var property = '1'
+//     return {
+//         getProperty: function () {
+//             return property;
+//         },
+//         setProperty: function (value) {
+//             property = value;
+//         }
+//     };
+// })
--- server/server.js
+++ server/server.js
@@ -17,37 +17,41 @@ var Message = mongoose.model('message', {
 
 //logged in users
 var numUsers = 0;
+var numSockets = 0;
+var loggedUser = "Guest"
 
-//io
-io.on('connection', function (socket) {
+
+io.on('connection', function(socket) {
     var addedUser = false
     var userToken = null
-    //When a request to the api is being done, console.log GetMessages
-    console.log('user connected')
+        //When a request to the api is being done, console.log GetMessages
+        ++numSockets
+    console.log("Sockets: " + numSockets, " | Users: " + numUsers)
 
-    socket.on('user token', function(data){
+    socket.on('user token', function(data) {
         console.log(data)
     })
 
     //start API
     app.get('/api/user', GetUsers)
+
     function GetUsers(req, res) {
-        User.find({}).exec(function (err, result) {
+        User.find({}).exec(function(err, result) {
             console.log(result)
             res.send(result)
         })
     }
     app.get('/api/message', GetMessages)
+
     function GetMessages(req, res) {
-        Message.find({}).exec(function (err, result) {
-            console.log(result)
+        Message.find({}).exec(function(err, result) {
             res.send(result)
         })
     }
 
-    socket.on('register user', function (username, password) {
+    socket.on('register user', function(username, password) {
         console.log('regUser: ' + username, 'regPassword: ' + password)
-        //Save user in MongoDB
+            //Save user in MongoDB
         var user = new User({
             username: username,
             password: password
@@ -56,16 +60,17 @@ io.on('connection', function (socket) {
 
     });
 
-    socket.on('login attempt', function (username, password) {
-        console.log('loginUser: ' + username, 'loginPassword: ' + password)
-        User.findOne({ username: username, password: password }, function (err, user) {
+    socket.on('login attempt', function(username, password) {
+        User.findOne({ username: username, password: password }, function(err, user) {
             if (user) {
                 if (addedUser) return;
-
-                console.log(user.username + ' logged in')
                 socket.username = username
-                ++numUsers
+                    ++numUsers
                 addedUser = true;
+                loggedUser = user.username
+                console.log(loggedUser + ' logged in')
+                console.log("Sockets: " + numSockets, " | Users: " + numUsers)
+
                 //io.sockets.emit('user logged in', socket.username)
                 socket.emit('login', {
                     username: socket.username,
@@ -75,31 +80,50 @@ io.on('connection', function (socket) {
                     username: socket.username,
                     numUsers: numUsers
                 })
-            }
-            else {
+            } else {
                 console.log('wrong credentials')
-                //send info wrong credentials
                 socket.emit('wrong credentials')
             }
         })
     })
 
-    socket.on('send message', function (data) {
+    socket.on('send message', function(data) {
         console.log(socket.username + ': ' + data)
-        //Save user in MongoDB
+            //Save user in MongoDB
         var message = new Message({
             username: socket.username,
             message: data,
         })
         message.save()
-        //socket.emit('emit message', message)
         io.sockets.emit('emit message', {
             username: socket.username,
             message: data
         })
     })
 
-    socket.on('disconnect', function () {
+    socket.on('log off', function(data) {
+        console.log(loggedUser + " logged out")
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+        console.log("Sockets: " + numSockets, " | Users: " + numUsers)
+
+        socket.username = "Guest"
+        addedUser = false
+
+        socket.broadcast.emit('log off event', numSockets, numUsers)
+    })
+
+    socket.on('disconnect', function() {
+        console.log(loggedUser + " logged out")
+            --numSockets
+        console.log("Sockets: " + numSockets, " | Users: " + numUsers)
         if (addedUser) {
             --numUsers;
 
@@ -116,14 +140,13 @@ io.on('connection', function (socket) {
 app.use(express.static(__dirname + '/../client'));
 
 //start server
-server.listen(port, function () {
+server.listen(port, function() {
     console.log('Server listening on port %d', port)
 })
 
 //conenct to Mongoose
-mongoose.connect('mongodb://localhost:27017/chat', function (err, db) {
+mongoose.connect('mongodb://localhost:27017/chat', function(err, db) {
     if (!err) {
         console.log('connected to mongo')
-    }
-    else (console.log(err))
+    } else(console.log(err))
 })
